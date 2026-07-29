595e4c50bbc3f1a0499c63ed1d186703fd68cc33
ukevine123 <ukevine239@gmail.com>
2026-07-29 10:56:05 +0200

    student detail dialog

--- Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb46d8c42e4dd35ebc91354107062822c696485")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24427bd503774b066828ed76791f258de382c163")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
